584d2cdcb0341717c064dda815abc706bcf73bdc
JESUS SANCHEZ <60315388064@senacrs.edu.br>
2026-08-03 13:46:09 -0300

    configuração dos arquivos

--- js/api.js
+++ js/api.js
@@ -1,23 +1,67 @@
-const baseApi = 'http://localhost:3000'
+const baseApi = 'https://uc14-backend-1.onrender.com'
 
 document.addEventListener("DOMContentLoaded", async () => {
-    const formLogin = document.getElementById('fromLogin')
+    const formLogin = document.getElementById('formLogin')
+    const formRegister = document.getElementById('formRegister')
 
-    if(formLogin){
-        formLogin.addEventListener('submit', async() => {
+    if (formLogin) {
 
-            const email = document.getElementById('email')
-            const password = document.getElementById('password')
+        formLogin.addEventListener('submit', async(e) => {
+            e.preventDefault()
+
+            const email = document.getElementById('email').value
+            const password = document.getElementById('password').value
 
             const res = await fetch(`${baseApi}/login`, {
                 headers: { "Content-Type": "application/json" },
                 method: 'POST',
                 credentials: "include",
-                body: {
-                    emial: JSON
-                }
+                body: JSON.stringify({ email: email, password: password })
             })
-            
+
+            console.log(res)
+
+            const data = await res.json()
+
+            if (!data.success) {
+                alert(`Algo deu errado: ${data.message}`)
+            } else {
+                alert(data.message)
+                window.location.href = './home.html'
+            }
+
         })
     }
+
+    if (formRegister) {
+
+        formRegister.addEventListener('submit', async(e) => {
+            e.preventDefault()
+            
+            const name = document.getElementById('name').value
+            const email = document.getElementById('email').value
+            const password = document.getElementById('password').value
+            const confirmPassword = document.getElementById("confirmPassword").value
+
+            if (password !== confirmPassword) {
+                alert('As senhas não coincidem')
+                return
+            }
+
+            const res = await fetch(`${baseApi}/register`, {
+                headers: { "Content-Type": "application/json" },
+                method: 'POST',
+                credentials: "include",
+                body: JSON.stringify({ name: name, email: email, password: password })
+            })
+
+            console.log(res)
+
+            const data = await res.json()
+
+            alert(data.message)
+
+        })
+    }    
+
 })
